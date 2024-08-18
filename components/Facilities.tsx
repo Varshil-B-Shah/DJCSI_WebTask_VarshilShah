f2f3b436a437cdde2c7534f3b26ebcca914ed69f
Varshil-B-Shah <varshil4247shah@gmail.com>
2024-08-18 22:08:20 +0530

    animation in star-rating added

--- components/Facilities.tsx
+++ components/Facilities.tsx
@@ -1,9 +1,18 @@
 const Facilities = () => {
-    return ( 
-        <div className="mt-28">
-            <div></div>
+  return (
+    <div className="mt-28">
+      <div className="px-16">
+        <div className="flex">
+          <div className="flex flex-col gap-7">
+            <h2 className="text-4xl font-semibold w-[400px] leading-[43px]">We do our best facilities provide you</h2>
+            <p className="text-sm text-[#6b6a6a] w-[330px] leading-[24px]">Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry.Lorem Ipsum has been the industry's standard dummy</p>
+            <button className="text-white bg-[] py-2 px-4 rounded-3xl">Contact Now</button>
+          </div>
         </div>
-     );
-}
- 
+      </div>
+    </div>
+  );
+};
+
 export default Facilities;
